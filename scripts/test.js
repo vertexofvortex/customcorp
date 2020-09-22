e2a441718c983c18e0e7263451434981b3245e6c
vertexofvortex <vertexofvortex@gmail.com>
2020-09-22 21:45:37 +0300

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,11 @@ try {
     document.querySelectorAll('.entry-content').forEach(function(current) {
         current.innerHTML = current.innerHTML.replace(/\[accent\]/, '<span class="bbAccent">');
         current.innerHTML = current.innerHTML.replace(/\[\/accent\]/, '</span>');
+        
         current.innerHTML = current.innerHTML.replace(/\[hr\]/, '<div class="bbHr"></div>');
+
+        current.innerHTML = current.innerHTML.replace(/\[s\]/, '<span class="bbStriketrough">');
+        current.innerHTML = current.innerHTML.replace(/\[\/s\]/, '</span>');
     });
 
 } catch {}
