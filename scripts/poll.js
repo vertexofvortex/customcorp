fc279b3b4c75246209e1d16c7c8db3c45449c789
Nikolay <31506708+vertexofvortex@users.noreply.github.com>
2021-02-12 13:02:13 +0300

    Update poll.js

--- scripts/poll.js
+++ scripts/poll.js
@@ -1,10 +1,15 @@
 if (window.location.toString().includes(`viewtopic.php?`)) {
-    var entryContent = document.querySelector('.entry-content');
-    var entryContentText = entryContent.innerHTML;
+    var entryContent = document.querySelectorAll('.entry-content');
 
-    if (entryContentText.includes(`[poll]`)) {
-        
-    entryContentTextReplaced = entryContentText.replaceAll(/(\[poll\])(.*?)(\[\/poll\])/g, `<iframe src="https://www.strawpoll.me/embed_1/$2" style="width: 100%; height: 350px; border: 0; display: block" seamless>Loading poll...</iframe>`);
-    entryContent.innerHTML = entryContentTextReplaced;
+    if (document.querySelector('.main-content').innerHTML.includes(`[poll]`)) {
+        entryContent.forEach(function(element) {
+            var entryContentText = element.innerHTML;
+
+            if (entryContentText.includes(`[poll]`)) {
+                entryContentTextReplaced = entryContentText.replaceAll(/(\[poll\])(.*?)(\[\/poll\])/g, `<iframe src="https://www.strawpoll.me/embed_1/$2" style="width: 100%; height: 350px; border: 0; display: block" seamless>Loading poll...</iframe>`);
+                entryContentTextReplaced = `<div style="width: calc(100% + 2.5em - 2px); background: #f4c9c1; padding: 10px; margin: -0.5em -1em 1em -1em; box-sizing: border-box">В сообщении используется экспериментальная функция, из-за которой могут возникать ошибки. Просьба о недочётах сообщать <a href="https://forum.minecorp.ru/viewtopic.php?id=29714">сюда</a>.</div>` + entryContentTextReplaced;
+                element.innerHTML = entryContentTextReplaced;
+            }
+        });
     }
 }
